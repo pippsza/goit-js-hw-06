f47ef2cced65fe424be70657636abade9c6dd56e
pippsza <bastahov18@gmail.com>
2024-12-08 01:08:12 +0200

    first commit

---
+++ js/lkafldas.py
@@ -0,0 +1,8 @@
+first_number = int(input("Please ENTER your first number: "))
+second_number = int(input("Please ENTER your second  number: "))
+
+
+print("Summ is: ", first_number + second_number,
+      "\nDifference is: ", first_number - second_number,
+      "\nProduct is: ", first_number * second_number,
+      "\nDivision is: ", first_number / second_number)
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {}
+  padEnd(str) {}
+  padStart(str) {}
+  padBoth(str) {}
+}
